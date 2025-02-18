e6a8799ff183c9d4a6f9339fe9156b3691d2dffa
Heber Lazo <heberlazo@gmail.com>
2025-02-18 12:09:29 -0500

    added sessions and seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+
+  has_secure_password
 end
